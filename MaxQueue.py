900a282386bd3fde2d5397f9d94921a935dbe5e2
steph lee <40439815+disappointment1738@users.noreply.github.com>
2023-02-26 22:41:22 -0800

    imported MaxQueue.py

---
+++ MaxQueue.py
@@ -0,0 +1,121 @@
+from SLLQueue import SLLQueue
+from DLLDeque import DLLDeque
+
+
+class MaxQueue(SLLQueue):
+    def __init__(self):
+        SLLQueue.__init__(self)
+        self.max_deque = DLLDeque()  # NOTE: DLLDeque implements the Deque interface but also inherits all methods from DLList 
+
+    def add(self, x : object):
+	"""
+	adds an element to the end of this max queue
+	INPUT: x the element to add
+	"""
+        pass
+
+    def remove(self) -> object:
+	"""
+	removes and returns the element at the head of the max queue
+	"""
+        pass
+
+    def max(self):
+	"""
+	returns the maximum element stored in the queue
+	"""
+        return self.max_deque.get(0)
+
+
+
+"""
+# TESTER
+mq = MaxQueue()
+mq.add(3)
+print("Added:", 3)
+print("MaxQueue contents:", mq)
+print("Max Dequeu contents", mq.max_deque)
+print("Max element", mq.max(), "\n\n")
+
+mq.add(2)
+print("Added:", 2)
+print("MaxQueue contents:", mq)
+print("Max Dequeu contents", mq.max_deque)
+print("Max element", mq.max(), "\n\n")
+
+mq.add(1)
+print("Added:", 1)
+print("MaxQueue contents:", mq)
+print("Max Dequeu contents", mq.max_deque)
+print("Max element", mq.max(), "\n\n")
+
+mq.add(4)
+print("Added:", 4)
+print("MaxQueue contents:", mq)
+print("Max Dequeu contents", mq.max_deque)
+print("Max element", mq.max(), "\n\n")
+
+r = mq.remove()
+print("Removed element:", r)
+print("MaxQueue contents:", mq)
+print("Max Dequeu contents", mq.max_deque)
+print("Max element", mq.max(), "\n\n")
+
+r = mq.remove()
+print("Removed element:", r)
+print("MaxQueue contents:", mq)
+print("Max Dequeu contents", mq.max_deque)
+print("Max element", mq.max(), "\n\n")
+
+r = mq.remove()
+print("Removed element:", r)
+print("MaxQueue contents:", mq)
+print("Max Dequeu contents", mq.max_deque)
+print("Max element", mq.max(), "\n\n")
+
+mq.add(8)
+print("Added:", 8)
+print("MaxQueue contents:", mq)
+print("Max Dequeu contents", mq.max_deque)
+print("Max element", mq.max(), "\n\n")
+
+mq.add(3)
+print("Added:", 3)
+print("MaxQueue contents:", mq)
+print("Max Dequeu contents", mq.max_deque)
+print("Max element", mq.max(), "\n\n")
+
+mq.add(5)
+print("Added:", 5)
+print("MaxQueue contents:", mq)
+print("Max Dequeu contents", mq.max_deque)
+print("Max element", mq.max(), "\n\n")
+
+mq.add(4)
+print("Added:", 4)
+print("MaxQueue contents:", mq)
+print("Max Dequeu contents", mq.max_deque)
+print("Max element", mq.max(), "\n\n")
+
+mq.add(1)
+print("Added:", 1)
+print("MaxQueue contents:", mq)
+print("Max Dequeu contents", mq.max_deque)
+print("Max element", mq.max(), "\n\n")
+
+mq.add(6)
+print("Added:", 6)
+print("MaxQueue contents:", mq)
+print("Max Dequeu contents", mq.max_deque)
+print("Max element", mq.max(), "\n\n")
+
+
+while mq.size() > 0:
+    r = mq.remove()
+    print("Removed element:", r)
+    print("MaxQueue contents:", mq)
+    print("Max Dequeu contents", mq.max_deque)
+    if mq.size() > 0:
+        print("Max element", mq.max(), "\n\n")
+        
+"""
