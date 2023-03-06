42e9fb688da6362dc1ec6089ee751c0388140146
steph lee <40439815+disappointment1738@users.noreply.github.com>
2023-03-06 00:00:28 -0800

    implemented maxQueue

--- pa 274/MaxQueue.py
+++ pa 274/MaxQueue.py
@@ -12,15 +12,28 @@ class MaxQueue(SLLQueue):
         adds an element to the end of this max queue
         INPUT: x the element to add
         """
-        #todo
-        pass
+        # add to SLLQueue
+        super().add(x)
+        self.max_deque.add_last(0)
+        if x > self.max():
+            self.max_deque = DLLDeque()
+            self.max_deque.add_first(x)
+        else:
+            # add to max_deque
+            self.max_deque.add_last(x)
+            while self.max_deque.n > 1 and x > self.max_deque.dummy.prev.x:
+                self.max_deque.remove_last()
 
     def remove(self) -> object:
         """
         removes and returns the element at the head of the max queue
         """
-        #todo
-        pass
+        removedValue = super().remove() #removes from head of SLL
+        if self.max_deque.n == 0:
+            raise IndexError()
+        if removedValue == self.max(): # checks if the max was removed
+            self.max_deque.remove_first() #removes head of DLL
+        return removedValue
 
     def max(self):
         """
