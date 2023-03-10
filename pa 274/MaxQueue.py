3cb523ed8745d8096556e5c3f33326a90b3ac2d0
steph lee <40439815+disappointment1738@users.noreply.github.com>
2023-03-09 21:35:36 -0800

    had to fix indentation error for maxQueue

--- pa 274/MaxQueue.py
+++ pa 274/MaxQueue.py
@@ -5,42 +5,29 @@ from DLLDeque import DLLDeque
 class MaxQueue(SLLQueue):
     def __init__(self):
         SLLQueue.__init__(self)
-        self.max_deque = DLLDeque()  # NOTE: DLLDeque implements the Deque interface but also inherits all methods from DLList 
-
-    def add(self, x : object):
-	    """
-	    adds an element to the end of this max queue
-	    INPUT: x the element to add
-	    """
-        # add to SLLQueue
-        super().add(x)
-        self.max_deque.add_last(0)
-        if x > self.max():
-            self.max_deque = DLLDeque()
-            self.max_deque.add_first(x)
-        else:
-            for i in range(1, self.max_deque.n + 1):
-                if x > self.max_deque.get(i):
-                    self.max_deque.add(i, x)
-                if x > self.max_deque.dummy.prev.x:
-                    self.max_deque.remove_last()
+        # NOTE: DLLDeque implements the Deque interface but also inherits all methods from DLList
+        self.max_deque == DLLDeque()
+    
+    def add(self, x: object):
+        """
+        adds an element to the end of this max queue
+        """
+        #todo
+        pass
 
     def remove(self) -> object:
-	    """
-	    removes and returns the element at the head of the max queue
-	    """
-        removedValue = super().remove() #removes from head of SLL
-        if self.max_deque.n == 0:
-            raise IndexError()
-        if removedValue == self.max(): # checks if the max was removed
-            self.max_deque.remove_first() #removes head of DLL
-        return removedValue
+        """
+        removes and returns the element at the head of the max queue
+        """
+        # todo
+        pass
 
     def max(self):
-	    """
-	    returns the maximum element stored in the queue
-	    """
+        """
+        returns the maximum element stored in the queue
+        """
         return self.max_deque.get(0)
+
 
 
 
