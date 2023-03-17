7c647d8b1601a21357cae823b6269baa4535aed4
steph lee <40439815+disappointment1738@users.noreply.github.com>
2023-03-17 12:34:45 -0700

    implemented ChainedHashTable (not tested)

--- pa 274/ChainedHashTable.py
+++ pa 274/ChainedHashTable.py
@@ -30,20 +30,61 @@ class ChainedHashTable(Set):
         return self.n
 
     def find(self, key: object) -> object:
-        # todo
-        pass
+        # determine the bin of the list where the item is stored
+        bin = self._hash(key)
+        # iterate over the list at that bin index and check if the keys match (return value if they do)
+        for i in range(self.t[bin].size() - 1):
+            if self.t[bin].get(i).value:
+                return self.t[bin].get(i).value
+        # return None if the loop doesn't return anything, then none of the keys match
+        return None
 
     def add(self, key: object, value: object):
-        # todo
-        pass
+        # check the precondition: if item with key exists, return false
+        if self.find(key)!= None:
+            return False
+        # check the invariant: do we need to resize?
+        if self.n == len(self.t):
+            self.resize()
+        # bin index
+        bin = self._hash(key)
+        # create a new node with key and val
+        item = self.Node(key, value)
+        # insert the new node to the start of the list the bin memory index (t[has_code(key)])
+        self.t[bin].add(0, item)
+        # increment num of elements by 1
+        self.n += 1
+        return True
 
     def remove(self, key: int) -> object:
-        # todo
-        pass
+        # find the bin index where the node with the given key should be stored
+        bin = self._hash(key)
+        # iterate over the nodes in that bin, and check if the item key matches the given key
+        for i in range(self.t[bin].size() - 1):
+            self.t[bin].remove(i)
+            self.n -= 1 # decrement num of elements by 1
+            # check the invariant
+            if len(self.t) > 3 * self.n: 
+                self.resize()
+        # return false if nothing matched because there is nothing to remove
+        return False
 
     def resize(self):
-        # todo
-        pass
+        # check the num of items in the table is the same as the num of bins.
+        # if same, double the num of bins
+        if self.n == len(self.t):
+            self.d += 1
+        # otherwise, create temp with half of num of bins
+        else:
+            self.d -= 1
+        # create new temp table
+        temp = self.alloc_temp(2 ** self.d)
+        # copy every list in table t into the table temp
+        for j in range(len(self.t) - 1):
+            for i in range(self.t[j].size() - 1):
+                bin = self._hash(self.t[j].get(i).key)
+        # reassign t to temp
+        self.t = temp
 
     def __str__(self):
         s = "["
