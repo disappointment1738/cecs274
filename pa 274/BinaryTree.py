5a5cd537f710671b0dc263b92c1c2b09aa2c041f
steph lee <40439815+disappointment1738@users.noreply.github.com>
2023-04-02 13:23:46 -0700

    implemented BT

--- pa 274/BinaryTree.py
+++ pa 274/BinaryTree.py
@@ -32,47 +32,106 @@ class BinaryTree(Tree):
         self.r = None
 
     def depth(self, u: Node) -> int:
-        # todo
-        pass
+        # check the precondition: is u nil?
+        if u is None:
+            return -1
+        # keep track of edges in the path from node u to the root
+        d = 0
+        current_node = u
+        # move up the tree until u is the root
+        while current_node == self.r:
+            current_node = current_node.parent
+            d += 1 # increment edge by 1
+        return d
 
     def height(self) -> int:
         return self._height(self.r)
 
     def _height(self, u: Node) -> int:
-        # todo
-        pass
+        # base case: is u nil?
+        if u is None:
+            return -1
+        # recursive (reduction) step
+        return 1 + max(self._height(u.left), self._height(u.right))
 
     def size(self) -> int:
         return self._size(self.r)
 
     def _size(self, u: Node) -> int:
-        # todo
-        pass
+        # base case: is u nil?
+        if u is None:
+            return 0
+        # recursive (reduction) step
+        return 1 + self._size(u.left) + self._size(u.right)
 
     def bf_order(self):
-        # todo
-        pass
+        nodes = [] # empty list
+        q = SLLQueue.SLLQueue() # empty queue
+        # add root to queue
+        if self.r is not None:
+            q.add(self.r)
+        # while queue is not empty 
+        while q.size() > 0:
+            # remove next node and add to nodes
+            u = q.remove()
+            nodes.append(u)
+            # check the children
+            if u.left is not None:
+                q.add(u.left)
+            if u.right is not None:
+                q.add(u.right)
+        return nodes
 
     def in_order(self) -> list:
         return self._in_order(self.r)
 
     def _in_order(self, u: Node) -> list:
-        # todo
-        pass
+        nodes = []
+        # check if u.left exists
+        if u.left is not None:
+            # handles left side
+            nodes.extend(self._in_order(u.left))
+        # add u to nodes
+        nodes.append(u)
+        # check if u.right exists
+        if u.right is not None:
+            # handles right side
+            nodes.extend(self._in_order(u.right))
+        return nodes
 
     def post_order(self) -> list:
         return self._post_order(self.r)
 
     def _post_order(self, u: Node):
-        # todo
-        pass
+        nodes = []
+        # check if u.left exists
+        if u.left is not None:
+            # handles left side
+            nodes.extend(self._post_order(u.left))
+        # check if u.right exists
+        if u.right is not None:
+            # handles right side
+            nodes.extend(self._post_order(u.right))
+        # add u to nodes
+        nodes.append(u)
+        return nodes
 
     def pre_order(self) -> list:
         return self._pre_order(self.r)
 
     def _pre_order(self, u: Node):
-        # todo
-        pass
+        nodes = []
+        # add u to nodes
+        nodes.append(u)
+        # check if u.left exists
+        if u.left is not None:
+            # handles left side
+            nodes.extend(self._pre_order(u.left))
+        # check if u.right exists
+        if u.right is not None:
+            # handles right side
+            nodes.extend(self._pre_order(u.right))
+        return nodes
 
     def __str__(self):
         nodes = self.bf_order()
