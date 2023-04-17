10db8bfc33fe5c746ecee1a689f1f869cfa4a9fc
steph lee <40439815+disappointment1738@users.noreply.github.com>
2023-04-17 16:05:56 -0700

    some heaps, sorts, and searches

--- pa 274/BinaryHeap.py
+++ pa 274/BinaryHeap.py
@@ -8,24 +8,21 @@ def left(i: int) -> int:
     """
     helper method; returns the index of the left child of the element at index i
     """
-    # todo
-    pass
+    return 2 * i + 1
 
 
 def right(i: int) -> int:
     """
     helper method; returns the index of the right child of the element at index i
     """
-    # todo
-    pass
+    return 2 * (i + 1)
 
 
 def parent(i: int) -> int:
     """
     helper method; returns the index of the parent of the element at index i
     """
-    # todo
-    pass
+    return (i - 1) // 2
 
 
 def _new_array(n: int) -> np.array:
@@ -41,16 +38,45 @@ class BinaryHeap(Queue, Tree):
         self.n = 0
 
     def add(self, x: object):
-        # todo
-        pass
+        # check invariant 
+        if len(self.a) == self.n:
+            self._resize()
+        # append x to heap
+        self.a[self.n] = x
+        # move element to its correct position in heap
+        self._bubble_up_last()
+        return True
 
     def remove(self):
-        # todo
-        pass
+        # check precodition
+        if self.n == 0:
+            raise IndexError
+        # store value of root
+        x = self.a[0]
+        # overwrite root with the last element
+        self.a[0] = self.a[self.n - 1]
+        # decrement num of nodes by 1
+        self.n -= 1
+        # move root node to the correct position
+        self._trickle_down_root()
+        # check invariant
+        if 3 * self.n < len(self.a):
+            self._resize()
+        # return removed value
+        return x 
 
     def depth(self, u) -> int:
-        # todo
-        pass
+        # check the precondition: is u nil?
+        if u is None:
+            return -1
+        # keep track of edges in the path from node u to the root
+        d = 0
+        current_node = u
+        # move up the tree until u is the root
+        while current_node != self.r:
+            current_node = current_node.parent
+            d += 1 # increment edge by 1
+        return d
 
     def height(self) -> int:
         # todo
@@ -80,16 +106,37 @@ class BinaryHeap(Queue, Tree):
         return self.a[0]
 
     def _bubble_up_last(self):
-        # todo
-        pass
+        i = self.n - 1
+        parent = parent(i)
+        while i > 0:
+            # swap parent and child
+            self.a[i], self.a[parent] = self.a[parent], self.a[i] 
+            i = parent # update index
+            parent = parent(i) # update the new parent
 
     def _resize(self):
-        # todo
-        pass
+        # create new array with capacity 2n
+        b = self._new_array(max(1, 2 * self.n))
+        # copy the elements from a to b
+        for k in range(0, self.n):
+            b[k] = self.a[k]
+        # assign a to the new array
+        self.a = b
 
     def _trickle_down_root(self):
-        # todo
-        pass
+        i = 0 # current index of element (at root rn)
+        # find children
+        left = left(i)
+        right = right(i)
+        # when i is less than some stuff and when a[i] is larger than its kids
+        while i < self.n or i < left or i < right and self.a[left] < self.a[i] or self.a[right] < self.a[i]:
+            # find the minimum element in a[i], a[left], a[right]
+            minIdx = min(left, right, i)
+            self.a[i], self.a[minIdx] = self.a[minIdx], self.a[i]
+            i = minIdx # update index
+            # update children for new index i 
+            left = left(i)
+            right = right(i)
 
     def __str__(self):
         return str(self.a[0:self.n])
--- pa 274/algorithms.py
+++ pa 274/algorithms.py
@@ -5,23 +5,67 @@ from Interfaces import List
 
 
 def linear_search(a: List, x):
-    # todo
-    pass
+    for i in range(len(a)-1):
+        if a[i] == x:
+            return i
+    return -100
 
 
 def binary_search(a: List, x):
-    # todo
-    pass
+    left = 0
+    right = len(a) - 1
+    binary_help(a, left, right, x)
+
+
+def binary_help(a: list, left: int, right: int, x):
+    """
+    Helper method for binary_search that uses recursion to search through the list.
+    """
+    while left <= right:
+        mid = (left + right) // 2
+        if x == a[mid]:
+            return mid
+        elif x < a[mid]:
+            # update right
+            right = mid - 1
+            # search the first half
+            binary_help(a, left, right, x)
+        else:
+            # update left
+            left = mid + 1
+            # search the second half
+            binary_help(a, left, right, x)
+    return -100
 
 
 def _merge(a0: List, a1: List, a: List):
-    # todo
-    pass
+    i0 =0 # current index of a0
+    i1= 0 # current index of a1
+    for i in range(len(a)-1):
+        if i0 == len(a0)-1: # we copied all elements from a0
+            a[i] = a1[i1]
+            i1 += 1
+        elif i1 == len(a1)-1: # we copied all elements from a1
+            a[i] = a0[i0]
+            i0 += 1
+        elif a0[i0] <= a1[i1]:
+            a[i] = a0[i0]
+            i0 += 1
+        else:
+            a[i] = a1[i1]
+            i1 += 1
 
 
 def merge_sort(a: List):
-    # todo
-    pass
+    if len(a) <= 1:
+        return a # because it's already sorted
+    mid = len(a) // 2
+    a0 = a[:mid-1] # first half of array
+    a1 = a[mid:] # second half of array
+    # recusively sort
+    merge_sort(a0)
+    merge_sort(a1)
+    _merge(a0, a1, a)
 
 
 def _quick_sort_f(a: List, start, end):
