2eb1b9c9a60d014379f393f1cb88e72b092d343b
steph lee <40439815+disappointment1738@users.noreply.github.com>
2023-04-23 20:14:15 -0700

    untested

--- pa 274/algorithms.py
+++ pa 274/algorithms.py
@@ -8,19 +8,12 @@ def linear_search(a: List, x):
     for i in range(len(a)-1):
         if a[i] == x:
             return i
-    return -100
+    return None
 
 
 def binary_search(a: List, x):
     left = 0
     right = len(a) - 1
-    binary_help(a, left, right, x)
-
-
-def binary_help(a: list, left: int, right: int, x):
-    """
-    Helper method for binary_search that uses recursion to search through the list.
-    """
     while left <= right:
         mid = (left + right) // 2
         if x == a[mid]:
@@ -28,32 +21,29 @@ def binary_help(a: list, left: int, right: int, x):
         elif x < a[mid]:
             # update right
             right = mid - 1
-            # search the first half
-            binary_help(a, left, right, x)
         else:
             # update left
             left = mid + 1
-            # search the second half
-            binary_help(a, left, right, x)
-    return -100
+    return None
 
 
 def _merge(a0: List, a1: List, a: List):
-    i0 =0 # current index of a0
-    i1= 0 # current index of a1
-    for i in range(len(a)-1):
-        if i0 == len(a0): # we copied all elements from a0
-            a[i] = a1[i1]
-            i1 += 1
-        elif i1 == len(a1): # we copied all elements from a1
-            a[i] = a0[i0]
-            i0 += 1
-        elif a0[i0] <= a1[i1]:
-            a[i] = a0[i0]
-            i0 += 1
+    i = 0
+    # checks and merges lists together
+    while 0 < len(a0) and 0 < len(a1):
+        if a0[0] < a1[0]:
+            a[i] = a0.pop(0)
         else:
-            a[i] = a1[i1]
-            i1 += 1
+             a[i] = a1.pop(0)
+        i += 1
+    # when all elements of the other list are copied
+    while 0 < len(a0):
+         a[i] = a0.pop(0)
+         i += 1
+    while 0 < len(a1):
+        a[i] = a1.pop(0)
+        i += 1
+    return a
 
 
 def merge_sort(a: List):
@@ -63,38 +53,55 @@ def merge_sort(a: List):
     a0 = a[:mid] # first half of array
     a1 = a[mid:] # second half of array
     # recusively sort
-    merge_sort(a0)
-    merge_sort(a1)
-    _merge(a0, a1, a)
+    a0 = merge_sort(a0)
+    a1 = merge_sort(a1)
+    return _merge(a0, a1, a)
 
 
-def _quick_sort_f(a: List, start, end):
-    # call partition
-    pass
+def _quick_sort(a: List, start, end, p = True):
+    if start < end:
+        if p:
+            pIdx = partition(a, start, end)
+        else:
+            pIdx = partitionDes(a, start, end)
+        _quick_sort(a, start, pIdx - 1)
+        _quick_sort(a, pIdx + 1, end)
 
 
-def _quick_sort_r(a: List, start, end):
-    # swap pivot and the first element
-    # call partition
-    pass
-
-def partition(a: list, l: int, r: int):
-    pivot = a[l]
-    l += 1
+def partition(a: List, start, end):
+    pivot = a[start]
+    l = start + 1
+    r = end
     while l <= r:
         # loop for l
         while a[l] <= pivot:
-            # todo
-            pass
+            l += 1
         # loop for r
         while a[r] > pivot:
-            # todo
-            pass
-        # swap a[l] and a[r]
-        a[l], a[r] = a[r], a[l]
+            r -= 1
+        if l < r:
+            # swap a[l] and a[r]
+            a[l], a[r] = a[r], a[l]
     # swap pivot and a[r]
-    a[0], a[r] = a[r], a[0] # where a[0] is the pivot
-    return a
+    a[start], a[r] = a[r], a[start] # where a[start] is the pivot
+    return l
+
+def partitionDes(a: List, start, end):
+    """ 
+    Does the same thing as parition but it does it from right to left
+    """
+    pivot = a[end]
+    l = start
+    r = end - 1
+    while l <= r:
+        while a[l] >= pivot:
+            l += 1
+        while a[r] < pivot:
+            r -= 1
+        if l < r:
+            a[l], a[r] = a[r], a[l]
+    a[end], a[l] = a[l], a[end]
+    return l
 
 def quick_sort(a: List, p=True):
     """
@@ -102,7 +109,5 @@ def quick_sort(a: List, p=True):
     If parameter p is True, the quick sort algorithm uses a randomly chosen element from a as pivot.
     Otherwise, the quick sort algorithm uses the first element as pivot.
     """
-    if p:
-        _quick_sort_r(a, 0, a.size() - 1)
-    else:
-        _quick_sort_f(a, 0, a.size() - 1)
+    _quick_sort(a, 0, len(a) - 1)
+    return a
